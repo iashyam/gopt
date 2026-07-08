3e62c601f2da38a4f4be140b201264cf5c22b6fe
shyam@ubuntu-server <shyam10kwd@gmail.com>
2026-07-08 17:03:46 +0000

    hashing using sha256

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import ("fmt"
+		"crypto/sha256"
+		"encoding/hex"
+		)
 
 func main(){
 	fmt.Println("hello world");
+	input := "This is super secret"
+	fmt.Printf("Input: %s\n hash:%s\n ", input, hash(input))
+}
+
+
+func hash(input string) string{
+	
+	inputInBytes := []byte(input);
+	hash := sha256.Sum256(inputInBytes)
+
+	return hex.EncodeToString(hash[:])
 }
